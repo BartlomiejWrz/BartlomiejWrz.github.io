b0b1d836b365638241bfca184aae60bed8f0623e
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2018-09-12 23:25:04 +0200

    small changescd...

--- script/main.js
+++ script/main.js
@@ -2,10 +2,12 @@
 let scroller = document.getElementById("arrow");
 
 scroller.addEventListener("click", function(){
-    console.log("scrolled")
+    console.log("scrolled");
 });
-   
 
 
-    
-    
+function scroll_to(selector) {
+    $('html,body').animate({scrollTop: $(selector).offset().top}, 1000);
+    return false;
+} 
+<a href="javascript:void();" onclick="scroll_to('#docelowy_id');">link</a>
